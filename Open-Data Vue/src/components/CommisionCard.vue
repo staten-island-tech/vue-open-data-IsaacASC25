f4e2e41da2c883b387103345b1017c2569467431
Lunacywonders <Isaacn9394@gmail.com>
2026-03-17 22:38:22 -0400

    asd

--- Open-Data Vue/src/components/CommisionCard.vue
+++ Open-Data Vue/src/components/CommisionCard.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>
-
-    </div>
+  <div class="card">
+    <h3>{{ id }}</h3>
+  </div>
 </template>
 
 <script setup>
-
+defineProps({
+  civics: Object,
+  id: Number
+})
 </script>
 
 <style scoped>
-
+.card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin: 16px;
+  border-radius: 8px;
+}
 </style>
