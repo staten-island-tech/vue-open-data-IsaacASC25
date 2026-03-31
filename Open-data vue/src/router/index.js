242b8a6109ca41d5f18a34fcd98985d154836417
Lunacywonders <Isaacn9394@gmail.com>
2026-03-30 22:53:11 -0400

    asd

--- Open-data vue/src/router/index.js
+++ Open-data vue/src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LEPLanguages from '../views/LEPLanguages.vue'
-import LEPPopulation from '../views/LEPPopulation.vue'
+import LEPLanguages from '@/views/LEPLanguages.vue'
+import LEPPopulation from '@/views/LEPPopulation.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'LEPLanguages',
+      name: 'lepLanguages',
       component: LEPLanguages,
     },
     {
       path: '/LEPPopulation',
-      name: 'LEPPopulation',
+      name: 'lepPopulation',
       component: LEPPopulation,
     },
   ],
